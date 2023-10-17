5c973c439f0340d8890530eb8e224224528f0ff6
mahsahariri <mahsa.aslhariri@yahoo.com>
2023-10-16 23:09:07 -0700

    adds base file

---
+++ personal_village/lib/login/view/chatpage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:personal_village/utility/get_it_handler.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:formz/formz.dart';
+import 'package:personal_village/login/bloc/login_bloc.dart';
+
+import 'package:personal_village/values/constants.dart';
+import 'package:personal_village/values/routes.dart';
+
+
+class chatpage extends StatelessWidget {
+  const chatpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
